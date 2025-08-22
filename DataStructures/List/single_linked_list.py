e0edc24670c67c2e195a1923530dbef6d7c73773
SantiagoGR157 <s.gaitanr@uniandes.edu.co>
2025-08-22 16:58:02 -0500

    lab

---
+++ DataStructures/List/single_linked_list.py
@@ -0,0 +1,39 @@
+def new_list():
+    newlist = {
+        "first": None,
+        "last": None
+        "size": 0,
+    }
+
+    return newlist
+
+def get_element(my_list,pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos:
+        node = node["next"]
+        searchpos +=1
+    return node["info"]
+
+def is_present(my_list, element, cmp_function):
+    is_in_array = False
+    temp = my_list["first"]
+    count = 0
+    while not is_in_array and temp is not None:
+        if cmp_function(element, temp["info"]) == 0:
+            is_in_array = True
+        else:
+            temp = temp["next"]
+            count += 1
+    
+    if not is_in_array:
+        count = -1
+    return count
+
+def is_empty(my_list,e):
+    if my_list["size"] == 0:
+        respuesta = True
+    if my_list["size"] != 0:
+        respuesta = False
+    return respuesta
+
